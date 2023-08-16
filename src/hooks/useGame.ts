c1f182ed1ddc28de563e66ed45188c477751699d
Ahmed Hashim <aahashim@gmail.com>
2023-08-16 10:07:07 +0300

    Sort the games

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -17,6 +16,16 @@ export interface Game {
 }
 
 const useGame = (gameQuery: GameQuery) =>
-  useData<Game>("/games", { params: { gameQuery } }, [gameQuery]);
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+      },
+    },
+    [gameQuery]
+  );
 
 export default useGame;
