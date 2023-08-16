6af51ffc56b711b1cfc40593f1b1aa108ee99f47
Ahmed Hashim <aahashim@gmail.com>
2023-08-16 10:09:01 +0300

    Handle games with no image

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,4 +1,7 @@
+import noImage from "../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp";
+
 const optimizedImage = (url: string) => {
+  if (!url) return noImage;
   const newUrl = url.replace("/media/", "/media/crop/600/400/");
   return newUrl;
 };
